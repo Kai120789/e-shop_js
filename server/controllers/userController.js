50a91d1f84bff76ed64b8a9a477f4261bda6ebd9
Kai120789 <zoomer120789@gmail.com>
2025-01-28 17:22:22 +0300

    feat: user registration with JWT

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,31 @@
-const {User} = require('../models/models')
+const {User, Basket} = require('../models/models')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const ApiError = require('../error/ApiErrors')
 
-class UserController {
-    async registration(req, res) {
+const generateJWT = (id, email, role) => {
+    return jwt.sign(
+        {id, email, role}, 
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+    )
+}
 
+class UserController {
+    async registration(req, res, next) {
+        const {email, password, role} = req.body
+        if (!email || !password) {
+            return next(ApiError.badRequest('Некорректный email или password'))
+        }
+        const candidate = await User.findOne({where: {email}})
+        if (candidate) {
+            return next(ApiError.badRequest('Пользователь уже существует'))
+        }
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({email, role, password: hashPassword})
+        const basket = Basket.create({userId: user.id})
+        const token = generateJWT(user.id, user.email, user.role)
+        return res.json({token})
     }
 
     async login(req, res) {
