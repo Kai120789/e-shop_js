53c5f1c369ea99be9a07a271c7f0f1897193b13c
Kai120789 <zoomer120789@gmail.com>
2025-01-28 20:26:23 +0300

    feat: client router and pages

---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Routes, Route, Navigate} from "react-router-dom"
+import { authRoutes, publicRoutes } from "../router";
+import { SHOP_ROUTE } from "../utils/consts";
+
+const AppRoute = () => {
+    const isAuth = false
+    return (
+        <Routes>
+            {isAuth && authRoutes.map(({path, Component}) => 
+                <Route key={path} path={path} element={<Component />} exact />
+            )}
+
+            {publicRoutes.map(({path, Component}) => 
+                <Route key={path} path={path} element={<Component />} exact />
+            )}
+
+            <Route path="*" element={<Navigate to={SHOP_ROUTE} replace />} />
+
+        </Routes>
+    )
+}
+
+export default AppRoute
