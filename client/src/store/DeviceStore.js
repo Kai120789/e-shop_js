dae731a162d1fb3866aa5b91b126bd9e8409cf98
Kai120789 <zoomer120789@gmail.com>
2025-01-30 01:25:46 +0300

    feat: added main page, types and brands

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -2,42 +2,68 @@ import { makeAutoObservable } from "mobx";
 
 export default class DeviceStore {
     constructor() {
-        this._types = [
+        this.types = [
             {id: 1, name: 'electronic'},
-            {id: 2, name: 'machines'}
+            {id: 2, name: 'machines'},
+            {id: 3, name: 'notebooks'},
+            {id: 4, name: 'tablets'},
         ]
-        this._brands = [
+        this.brands = [
             {id: 1, name: 'Apple'},
-            {id: 2, name: 'Samsung'}
+            {id: 2, name: 'Samsung'},
+            {id: 3, name: 'Xiaomi'},
+            {id: 4, name: 'Lenovo'},
         ]
-        this._devices = [
-            {id: 1, name: 'Iphone 12', price: 10000, rating: 5, img: '', brandId: 1, typeId: 1},
-            {id: 2, name: 'Iphone 12 pro', price: 15000, rating: 5, img: '', brandId: 1, typeId: 1}
+        this.devices = [
+            {id: 1, name: 'Iphone 12', price: 10000, rating: 5, img: 'https://cdn-icons-png.flaticon.com/512/9929/9929025.png', brandId: 1, typeId: 1},
+            {id: 2, name: 'Iphone 12 pro', price: 15000, rating: 5, img: 'https://cdn-icons-png.flaticon.com/512/9929/9929025.png', brandId: 1, typeId: 1},
+            {id: 3, name: 'Iphone 12', price: 10000, rating: 5, img: 'https://cdn-icons-png.flaticon.com/512/9929/9929025.png', brandId: 1, typeId: 1},
+            {id: 4, name: 'Iphone 12 pro', price: 15000, rating: 5, img: 'https://cdn-icons-png.flaticon.com/512/9929/9929025.png', brandId: 1, typeId: 1},
+            {id: 5, name: 'Iphone 12', price: 10000, rating: 5, img: 'https://cdn-icons-png.flaticon.com/512/9929/9929025.png', brandId: 1, typeId: 1},
+            {id: 6, name: 'Iphone 12 pro', price: 15000, rating: 5, img: 'https://cdn-icons-png.flaticon.com/512/9929/9929025.png', brandId: 1, typeId: 1}
         ]
+        this.selectedType = {}
+        this.selectedBrand = {}
         makeAutoObservable(this)
     }
 
+    setSelectedType(type) {
+        this.selectedType = type
+    }
+
+    setSelectedBrand(brand) {
+        this.selectedBrand = brand
+    }
+
     setTypes(types) {
-        this._types = types
+        this.types = types
     }
 
     setBrands(brands) {
-        this._brands = brands
+        this.brands = brands
     }
 
     setDevices(devices) {
-        this._devices = devices
+        this.devices = devices
     }
 
-    getTypes(types) {
-        return this._types
+    getSelectedType() {
+        return this.selectedType
     }
 
-    getBrands(brands) {
-        return this._brands
+    getSelectedBrand() {
+        return this.selectedBrand
     }
 
-    getDevices(devices) {
-        return this._devices
+    getTypes() {
+        return this.types
+    }
+
+    getBrands() {
+        return this.brands
+    }
+
+    getDevices() {
+        return this.devices
     }
 }
